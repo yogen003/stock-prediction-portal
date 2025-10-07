1dd50841dac6199e23c15fd98fb28b39d2ed1d85
yogen003 <yogen.chandrakar@gmail.com>
2025-10-07 12:52:43 +0530

    registration backend/api

---
+++ backend/accounts/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework import generics
+from  rest_framework.permissions import AllowAny
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
