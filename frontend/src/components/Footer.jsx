93981bc1f50a330a77c228e6e76fdd05a653acb9
yogen003 <yogen.chandrakar@gmail.com>
2025-10-07 11:40:08 +0530

    homepage react

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+   <footer className='footer py-3 my-3'>
+    <hr className='border-bottom'/>
+    <p className='text-light text-center'> &copy; 2025 - Built  with ❤️ Yogen Chandrakar </p>
+
+   </footer> 
+   </>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+    <div className='container '>
+        <div className='p-5 text-center bg-light-dark rounded'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light lead'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid laborum libero fuga ex harum, maiores eveniet quam ad sint neque odit in architecto sequi! Suscipit doloremque consequatur obcaecati soluta corporis.</p>
+            <Button text='Login' class='btn-outline-info'/>
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Main
